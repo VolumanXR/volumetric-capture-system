6efa200bc863b10ee60402f3ca3b701f730b89e8
denamu <dooonnis@gmail.com>
2025-01-30 11:42:40 +0100

    (modified) camera controller for utils camera_list.json

--- capture-software/camera/v09/master_camera_controller.py
+++ capture-software/camera/v09/master_camera_controller.py
@@ -18,9 +18,9 @@ from pathlib import Path
 
 
 # Configuration
-CAMERA_LIST_FILE = 'camera_list.json'
-#SCRIPT_DIR = Path(__file__).resolve().parent
-#CAMERA_LIST_FILE = os.path.join(SCRIPT_DIR.parent.parent.parent,  'utils','camera_list.json') 
+# CAMERA_LIST_FILE = 'camera_list.json'
+SCRIPT_DIR = Path(__file__).resolve().parent
+CAMERA_LIST_FILE = os.path.join(SCRIPT_DIR.parent.parent.parent,  'utils','camera_list.json') 
 MASTER_SETTINGS_FILE = 'camera_settings.json'
 
 # Default settings
